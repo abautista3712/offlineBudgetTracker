0b6deb33d81360ff6c47618be6f6338c473e4f7b
Abelard Bautista <abelardbautista@gmail.com>
2020-05-13 12:01:06 -0700

    Added IndexDB to handle saving of offline transactions.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,10 @@
-const FILES_TO_CACHE = ["/", "/index.html", "/styles.css", "/index.js"];
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/index.js",
+  "/db.js",
+];
 
 const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
